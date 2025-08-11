cb3ccb996cc445c621d5dfdf1d66af088de4dd5d
hridyar007 <hridyar007@gmail.com>
2025-08-11 17:23:52 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <assert.h>
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -7,15 +6,9 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
+            // BUG: uses minorColor[i] instead of minorColor[j]
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     return i * j;
-}
-
-void testPrintColorMap() {
-    std::cout << "\nPrint color map test\n"; 
-    int result = printColorMap();
-    assert(result == 25);
-    std::cout << "All is well (maybe!)\n";
 }
