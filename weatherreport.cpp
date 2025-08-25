3f85be98558a9db77c07b54c82b6959a098b5816
hridyar007 <hridyar007@gmail.com>
2025-08-25 23:14:32 +0530

    Update weatherreport.cpp

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -1,44 +1,12 @@
-#include <string>
-#include <iostream>
+#include "helpers.h"
 
-namespace WeatherSpace
-{
-    class IWeatherSensor {
-    public:
-        virtual double TemperatureInC() const = 0;
-        virtual int Precipitation() const = 0;
-        virtual int Humidity() const = 0;
-        virtual int WindSpeedKMPH() const = 0;
-    };
-
+namespace WeatherSpace {
+    // SensorStub for production/integration
     class SensorStub : public IWeatherSensor {
     public:
-        int Humidity() const override {
-            return 72;
-        }
-        int Precipitation() const override {
-            return 70;
-        }
-        double TemperatureInC() const override {
-            return 26;
-        }
-        int WindSpeedKMPH() const override {
-            return 52;
-        }
+        int Humidity() const override { return 72; }
+        int Precipitation() const override { return 70; }
+        double TemperatureInC() const override { return 26; }
+        int WindSpeedKMPH() const override { return 52; }
     };
-
-    std::string Report(const IWeatherSensor& sensor)
-    {
-        int precipitation = sensor.Precipitation();
-        std::string report = "Sunny Day";
-
-        if (sensor.TemperatureInC() > 25)
-        {
-            if (precipitation >= 20 && precipitation < 60)
-                report = "Partly Cloudy";
-            else if (sensor.WindSpeedKMPH() > 50)
-                report = "Alert, Stormy with heavy rain";
-        }
-        return report;
-    }
 }
