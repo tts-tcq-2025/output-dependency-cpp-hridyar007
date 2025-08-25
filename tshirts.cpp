d4552ae005b5b5d165cb584b6bc44e5df8e76c65
hridyar007 <hridyar007@gmail.com>
2025-08-25 23:15:08 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
+#include "helpers.h"
 
 char size(int cms) {
-    char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
-    }
-    return sizeName;
+    return classifyTShirtSize(cms);
 }
-
