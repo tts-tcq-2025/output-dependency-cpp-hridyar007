6c518778274fb8481169982107770c5da6b35532
hridyar007 <hridyar007@gmail.com>
2025-08-25 23:14:49 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,14 +1,9 @@
-#include <iostream>
+#include "helpers.h"
+#include <vector>
+#include <string>
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            // BUG: uses minorColor[i] instead of minorColor[j]
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
-        }
-    }
-    return i * j;
+    std::vector<std::string> majorColor = {"White", "Red", "Black", "Yellow", "Violet"};
+    std::vector<std::string> minorColor = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    return printTable(majorColor, minorColor);
 }
